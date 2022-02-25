356ea95029cf7a360e13f29e1004dba753747348
konradBalon <konrad.balon@gmail.com>
2022-02-25 23:00:28 +0100

    postgress settings py + HostFactory class in models

--- backend_core/host/models.py
+++ backend_core/host/models.py
@@ -1,6 +1,9 @@
 from django.contrib.auth import get_user_model
 from django.core.validators import MinLengthValidator
 from django.db import models
+import factory
+
+from users.models import CustomUser
 
 
 class Host(models.Model):
@@ -27,3 +30,10 @@ class Subtype(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class HostFactory(factory.Factory):
+    class Meta:
+        model = Host
+
+    user = CustomUser(factory.Sequence(lambda n: 'user%d' % n))
