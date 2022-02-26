04c562d5ee76ea8583cca1d607d4d193b5b93848
remigiuszpulit <remigiusz.pulit@gmail.com>
2022-02-26 22:14:08 +0100

    added language names in lang switcher

--- frontend/src/components/LangSwitcher.js
+++ frontend/src/components/LangSwitcher.js
@@ -1,32 +1,24 @@
+import { useTranslation } from 'react-i18next';
 import React, { useEffect, useState } from 'react';
 import i18n from '../i18n';
 
 function LangSwitcher() {
+    const { t, i18n } = useTranslation();
     const [lang, setLang] = useState('')
     const options = i18n.languages
     const change = (e) => {
         setLang(e.target.value)
-        console.log(e.target.value);
-        
-
     }
     useEffect(() => {
-        
-        console.log(options);
         i18n.changeLanguage(lang)
     }, [lang])
-
-
-
- 
-
 
 return (
     
 <div>
     <select value={lang} onChange={change} id="lang">
         {options.map((option)=> {
-            return (<option key={option}>{option}</option>)
+            return (<option key={option} value={option}>{t(option)}</option>)
         } )}
     </select>
 </div>
