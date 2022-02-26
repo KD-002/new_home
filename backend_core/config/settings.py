f1c9a1effb27e4ea4281095e66337c95de6d020e
Paweł Konior <pawel.konior@gmail.com>
2022-02-26 01:11:02 +0100

    fix database configuration

--- backend_core/config/settings.py
+++ backend_core/config/settings.py
@@ -88,11 +88,16 @@ DATABASES = {
     }
 }
 
+
 DATABASE_URL = os.environ.get('DATABASE_URL')
 db_from_env = dj_database_url.config(
     default=DATABASE_URL, conn_max_age=500, ssl_require=False
 )
 DATABASES['default'].update(db_from_env)
+print('*' * 50)
+print(DATABASES['default'])
+print(DATABASE_URL)
+print('*' * 50)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
