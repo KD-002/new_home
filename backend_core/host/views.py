56c473f9660667ff5deaa4f3b0b7a7c179cf9da5
Lidia Szewczyk <lidia.szewczyk@gmail.com>
2022-02-26 21:00:33 +0100

    Pagination added

--- backend_core/host/views.py
+++ backend_core/host/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 from rest_framework import generics
+from rest_framework.pagination import PageNumberPagination
 
 from .models import Host
 from .serializers import HostSerializer
 
 
+class TenPagination(PageNumberPagination):
+    page_size = 10
+
+
 class HostView(generics.ListAPIView):
     queryset = Host.objects.all()
     serializer_class = HostSerializer
+    pagination_class = TenPagination
