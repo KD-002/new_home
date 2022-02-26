eb0d21698eda12285e45b911ec85dc045ebf0798
Paweł Konior <pawel.konior@gmail.com>
2022-02-26 20:12:00 +0100

    react bootstrap base config, react router config and header styling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,18 @@
 import './App.css';
+import Button from 'react-bootstrap/Button';
+
+import {
+    BrowserRouter,
+    Routes,
+    Route,
+} from "react-router-dom";
+
 import {useState} from "react";
+import Header from "./components/header/Header";
+import {Container} from "react-bootstrap";
 
 function App() {
-    const [hosts, setHosts] = useState();
+    const [hosts, setHosts] = useState([]);
 
     const handleHosts = () => {
         fetch('api/v1/hosts')
@@ -13,10 +23,32 @@ function App() {
     }
 
     return (
-        <div className="App">
-            <button onClick={handleHosts}>Get all Hosts</button>
-            <pre>{JSON.stringify(hosts, null, 2)}</pre>
-        </div>
+        <>
+            <BrowserRouter>
+                <Header/>
+                <Routes>
+                    <Route path="/" element={<Container>
+                        <Button variant="primary" onClick={handleHosts}>
+                            Get Hosts
+                        </Button>
+                        <ul>
+                            {hosts.map(host => <li key={host.id}>
+                                <pre>{JSON.stringify(host, null, 2)}</pre>
+                            </li>)}
+                        </ul>
+                    </Container>}/>
+                    <Route path="/hosts" element={<Container>
+                        <h1>Hosts</h1></Container>}/>
+                    <Route path="/helps" element={<Container>
+                        <h1>Helps</h1></Container>}/>
+                    <Route path="/transports" element={<Container>
+                        <h1>Transports</h1></Container>}/>
+                    <Route path="*" element={<Container>
+                        <h1>404</h1></Container>}/>
+                </Routes>
+            </BrowserRouter>
+
+        </>
     );
 }
 
