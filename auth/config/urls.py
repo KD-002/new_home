1d29471118cdaf97b98f2d9cb5dbd871a4341437
Lukasz Hoszowski <hoszak@gmail.com>
2022-02-27 12:10:00 +0100

    NH-009_Auth_AllAuth

--- auth/config/urls.py
+++ auth/config/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
+    path('accounts/', include('allauth.urls')),
 ]
